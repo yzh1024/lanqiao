861624917300c24c779e1c7d22fd93ba64620560
yzh <yzh1024@aliyun.com>
2020-07-16 11:00:15 +0800

    完成了基础练习1、2、3、4、5

--- 2基础练习/src/_4数列特征_循环_累加.java
+++ 2基础练习/src/_4数列特征_循环_累加.java
@@ -1,2 +1,49 @@
+import java.util.Scanner;
+
+/*
+资源限制
+时间限制：1.0s   内存限制：256.0MB
+问题描述
+给出n个数，找出这n个数的最大值，最小值，和。
+输入格式
+第一行为整数n，表示数的个数。
+第二行有n个数，为给定的n个数，每个数的绝对值都小于10000。
+输出格式
+输出三行，每行一个整数。第一行表示这些数中的最大值，第二行表示这些数中的最小值，第三行表示这些数的和。
+样例输入
+5
+1 3 -2 4 5
+样例输出
+5
+-2
+11
+数据规模与约定
+1 <= n <= 10000。
+ */
 public class _4数列特征_循环_累加 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            a[i]=x;
+        }
+        int max = a[0];
+        int min = a[0];
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            if(a[i]>max){
+                max = a[i];
+            }
+            if(a[i]<min){
+                min = a[i];
+            }
+            sum+=a[i];
+        }
+        System.out.println(max);
+        System.out.println(min);
+        System.out.println(sum);
+    }
+
 }
--- 2基础练习/src/_5查找整数_循环_判断.java
+++ 2基础练习/src/_5查找整数_循环_判断.java
@@ -1,2 +1,42 @@
+import java.util.Scanner;
+
+/*
+资源限制
+时间限制：1.0s   内存限制：256.0MB
+问题描述
+给出一个包含n个整数的数列，问整数a在数列中的第一次出现是第几个。
+输入格式
+第一行包含一个整数n。
+第二行包含n个非负整数，为给定的数列，数列中的每个数都不大于10000。
+第三行包含一个整数a，为待查找的数。
+输出格式
+如果a在数列中出现了，输出它第一次出现的位置(位置从1开始编号)，否则输出-1。
+样例输入
+6
+1 9 4 8 3 9
+9
+样例输出
+2
+数据规模与约定
+1 <= n <= 1000。
+ */
 public class _5查找整数_循环_判断 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        int x = sc.nextInt();
+        int flag = -1;
+        for (int i = 0; i < n; i++) {
+            if(x==a[i]){
+                flag=i+1;
+                break;
+            }
+        }
+        System.out.println(flag);
+    }
+
 }
