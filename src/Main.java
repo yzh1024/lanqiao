fcf1dc14b6590d2d77b19a9f6d104fb43f107747
yzh <yzh1024@aliyun.com>
2020-07-17 15:05:36 +0800

    完成了基础练习10

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+       /* int n = new Scanner(System.in).nextInt();
+        String s = Integer.toHexString(n);
+        System.out.println(s.toUpperCase());*/
+        String s1 = "ABCDefgHiJkLMN";
+        System.out.println(s1.toLowerCase());//abcdefghijklmn
+       String s2 = "HELLO WORLD,你好啊AA，1oqa世界a";
+       System.out.println(s2.toLowerCase());//hello world,你好啊aa，1oqa世界a
 
     }
 }
