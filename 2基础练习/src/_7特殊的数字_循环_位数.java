823d6c580625e61e28db3ceac2a3b8485fcdc3e6
yzh <yzh1024@aliyun.com>
2020-07-17 13:59:59 +0800

    完成了基础练习6、7、8、9

--- 2基础练习/src/_7特殊的数字_循环_位数.java
+++ 2基础练习/src/_7特殊的数字_循环_位数.java
@@ -1,2 +1,24 @@
+/*
+资源限制
+时间限制：1.0s   内存限制：512.0MB
+问题描述
+　　153是一个非常特殊的数，它等于它的每位数字的立方和，即153=1*1*1+5*5*5+3*3*3。编程求所有满足这种条件的三位十进制数。
+输出格式
+　　按从小到大的顺序输出满足条件的三位十进制数，每个数占一行。
+ */
 public class _7特殊的数字_循环_位数 {
+    public static void main(String[] args) {
+        int ge;
+        int shi;
+        int bai;
+        for (int i = 100; i < 1000; i++) {
+            ge = i % 10;
+            shi = i /10%10;
+            bai = i/100;
+            if(i==ge*ge*ge+shi*shi*shi+bai*bai*bai){
+                System.out.println(i);
+            }
+        }
+
+    }
 }
